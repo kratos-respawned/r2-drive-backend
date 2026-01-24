2c9ab45998f63258df2c4973ac5a6397a6f236c8
Gaurav Bhandari <gaurav@Gauravs-MacBook-Pro.local>
2026-01-24 16:23:21 +0530

    better auth init

---
+++ drizzle/migrations/0001_romantic_taskmaster.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `test` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`title` text NOT NULL,
+	`content` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,24 @@
-import { betterAuth } from "better-auth";
 
-export const auth = betterAuth({
-  
-});
+
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { betterAuth } from 'better-auth';
+// import { betterAuthOptions } from './options';
+
+import * as schema from "../db/schema"; // Ensure the schema is imported
+import { drizzle } from 'drizzle-orm/d1';
+
+/**
+ * Better Auth Instance
+ */
+export const auth = (env: CloudflareBindings): ReturnType<typeof betterAuth> => {
+//   const sql = neon(env.DATABASE_URL);
+  const db = drizzle(env.r2_drive);
+
+  return betterAuth({
+    database: drizzleAdapter(db, { provider: 'sqlite' }),
+    baseURL: env.BETTER_AUTH_URL,
+    secret: env.BETTER_AUTH_SECRET,
+
+    // Additional options that depend on env ...
+  });
+};
